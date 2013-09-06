722de9729f26eb03d510d672130cc2964a415a19
Geoff Lehr <geoff.lehr@gmail.com>
2013-09-05 21:02:53 -0600

    Added CC0 info

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
+/***********
+*
+* redirectChecker - Checks 301 redirects from a CSV file
+*
+* Written in 2013 by Geoff Lehr jademystery@hotmail.com
+*
+* To the extent possible under law, the author(s) have dedicated all
+* copyright and related and neighboring rights to this software to the 
+* public domain worldwide. This software is distributed without any warranty. 
+*
+* You should have received a copy of the CC0 Public Domain Dedication along
+* with this software. 
+* If not, see <http://creativecommons.org/publicdomain/zero/1.0/>. 
+*
+************/
+
 package main
 
 import (
